c022e031401b583aba05864d4452037816c77465
tomitakecirno <padio_apple@yahoo.co.jp>
2016-10-24 15:02:10 +0900

    追加

--- AnsModule.py
+++ AnsModule.py
@@ -1,9 +1,10 @@
-import Config
+import config
 import math
+import random
 
 def EuclidDis(one,ano):
 	cal_sum = 0;
-	for i in range(Config.Define.DEM):
+	for i in range(config.Define.DEM):
 		cal_sum += (one[i] - ano[i])*(one[i] - ano[i]);
 		
 	return round(math.sqrt(cal_sum),1);
@@ -18,7 +19,7 @@ def List1(Pop):
       else:
       	TmpDis[i].append(50)
     #print(TmpDis[i])
-    for k in range(Config.Define.Kp):
+    for k in range(config.Define.Kp):
       TmpIndex = TmpDis[i].index(min(TmpDis[i]))
       Pop[i][3].append(TmpIndex)
       TmpDis[i][TmpIndex] = 50
@@ -35,7 +36,7 @@ def List2(Pop):
       else:
       	TmpDis[i].append(50)
     #print(TmpDis[i])
-    for k in range(Config.Define.Kp):
+    for k in range(config.Define.Kp):
       TmpIndex = TmpDis[i].index(min(TmpDis[i]))
       TmpList[i].append(TmpIndex)
       TmpDis[i][TmpIndex] = 50
@@ -48,9 +49,33 @@ def List2(Pop):
       else :
         pass
     #print(Pop[i][4])
-        
-def MGG(one,ano,Child):
+
+#ニッチ番号を割り当てる　未完成
+def SetNitch(kn,Pop,n):
+  #ニッチに属していなければニッチ番号を割り振る
+  if Pop[kn][2] == 0:
+    Pop[kn][2] = n
+		#近傍リストを見ていく
+    for j in range(len(Pop[kn][4])):
+      TmpNumber = Pop[kn][4][j]
+      SetNitch(TmpNumber,Pop,n)
+    return 1
+  else :
+    return 0
+
+def ExtensionXLM():
   pass
+  #TmpChild = [[[] for i in range(2)] for j in range(config.Define.Nc)]
+  #return TmpChild
+
+def KousaOf2Point(MainPare_n,SubPare_n,Pop):
+  pass
+  #TmpChild = [[[] for i in range(2)] for j in range(config.Define.Nc)]
+  #return TmpChild
+
+def test():
+  print(config.Pop)
+
 
 
 
--- Config.py
+++ Config.py
@@ -1,24 +1,41 @@
 from enum import IntEnum
 import random
+
+CountNitch = 0
+
 class Define(IntEnum):
-  Ns = 3    #初期集団数
+  Ns = 5     #初期集団数
   No = 30    #敵集団数
-  Np = 4     #親個体数
-  Nc = 5     #子個体数
+  Np = 3     #親個体数
+  Nc = 3     #子個体数
   DEM = 2    #次元数
   Kp = 3
   Ko = 3
   T = 1      #ステップサイズ
+  END = 1  #終了時間
 
 #Nimの山に関するクラス
 class Nim:
   _MtNumber = 4
   Stone = list([3]*_MtNumber)
   Eval = []
+  
+  def DataSet(NimData):
+    for x in range(NimData.Stone[0]+1):
+      for y in range(NimData.Stone[1]+1):
+        for z in range(NimData.Stone[2]+1):
+          for a in range(NimData.Stone[3]+1):
+            if bin(x^y^z^a)!='0b0':
+              NimData.Eval.append(1)
+            else :
+              NimData.Eval.append(0)
+  def test():
+    print(_MtNumber) 
+  #print(len(NimData.Eval))
 
 class Kotai:
   n = []
   win = 0
   nitch = 0
-  List1 = list([Define.Kp])
-  List2 = list([Define.Ko])
+  List1 = [[] for i in range(5)]
+  List2 = []
