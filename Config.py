3980633270b31eae9830018f31171b57a141788a
tomitakecirno <padio_apple@yahoo.co.jp>
2016-10-26 20:08:49 +0900

    追加

--- Config.py
+++ Config.py
@@ -12,10 +12,10 @@ class Define(IntEnum):
   Kp = 3
   Ko = 3
   T = 1      #ステップサイズ
-  END = 10  #終了時間
-  Poll1 = 2
-  Poll2 = 2
-  Poll3 = 2
+  END = 50  #終了時間
+  Poll1 = 5
+  Poll2 = 5
+  Poll3 = 5
 
 #Nimの山に関するクラス
 """
